6a372914be4249e4337fd8e263dac7f34c17221a
Furkankilic0 <kilic.furkan@student.atilim.edu.tr>
2026-09-09 16:37:42 +0300

    feat: backend ve frontend proje iskeleti kuruldu

---
+++ backend/src/main/java/com/kolaysoft/ctotracker/CtotrackerApplication.java
@@ -0,0 +1,13 @@
+package com.kolaysoft.ctotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtotrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtotrackerApplication.class, args);
+	}
+
+}
